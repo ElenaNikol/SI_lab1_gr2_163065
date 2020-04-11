a2e857855ea4710d48b2efec6ba16bc645b05c8a
ElenaNikol <elena.nikolovska98@yahoo.com>
2020-04-11 18:00:45 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -8,13 +8,58 @@ class Student {
 
 	//TODO constructor
 
-	//TODO seters & getters
+    public Student(String index, String firstName, String lastName, ArrayList<> grades) {
+        this.index = index;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grades = grades;
+    }
+    //TODO seters & getters
 
-	public double getAverage() {
-		//TODO
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getAverage() {
+        int br=0,zbir=0;
+        for (int i = 0; i < grades.size(); i++) {
+            zbir += grades[i];
+            br++;
+        }
+        return zbir/br;
 	}
 
 	public int ECTSCredits() {
-		//TODO
+        int br =0;
+        for (int i = 0; i < grades.size(); i++) {
+            if(grades[i]>5)
+            br++;
+        }
+        return br;
 	}
+
 }
