7d35dad40752b7054c23b907a1e64c3b230955f5
ElenaNikol <elena.nikolovska98@yahoo.com>
2020-04-11 18:42:54 +0200

    added Faculty file to branch and changes in students.csv

--- Student.java
+++ Student.java
@@ -63,3 +63,46 @@ class Student {
 	}
 
 }
+
+
+class Faculty{
+    List<Student> students = new ArrayList<>();
+    String ime;
+
+    public Faculty(List<Student> students, String ime) {
+        this.students = students;
+        this.ime = ime;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public void deleteStudent(String ime){
+        for (int i = 0; i < students.size() ; i++) {
+            if(students.firstName == ime){
+                students.delete(i);
+            }
+        }
+    }
+
+    public void deleteStudent(String ime){
+        double prosek = 0;
+        for (int i = 0; i < students.size() ; i++) {
+            prosek += students[i].getAverage();
+        }
+        return prosek/students.size();
+    }
+}
